9d47996b3ed31c954324d684f29f9b90b9d912e7
Toby <master.toby.webb@gmail.com>
2026-07-30 23:42:41 +0100

    Added a rough template for calendar

--- lib/views/calendar_view.dart
+++ lib/views/calendar_view.dart
@@ -1,1 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:moodle/constants.dart';
+import 'package:moodle/widgets/nav_drawer.dart';
 
+class CalendarView extends StatelessWidget {
+  const CalendarView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: moodleWhite,
+        foregroundColor: moodleTextDark,
+        elevation: 1,
+        titleSpacing: 0,
+        title: const Text(
+          'Calendar',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+        ),
+      ),
+      drawer: const NavDrawer(),
+      body: Container(
+        color: moodleBg,
+        padding: const EdgeInsets.all(24.0),
+        child: const Text(
+          'Calendar',
+          style: TextStyle(
+            fontSize: 36,
+            fontWeight: FontWeight.bold,
+            color: moodlePurple,
+          ),
+        ),
+      ),
+    );
+  }
+}
