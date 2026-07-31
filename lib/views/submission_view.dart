ba4f4c0c84d3d28fbf5c65da11bf31c6cef52782
Toby <master.toby.webb@gmail.com>
2026-07-31 09:11:52 +0100

    Making submission page a stateful widget

--- lib/views/submission_view.dart
+++ lib/views/submission_view.dart
@@ -2,9 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:moodle/constants.dart';
 import 'package:moodle/widgets/nav_drawer.dart';
 
-class SubmissionView extends StatelessWidget {
+class Assignment {
+  final String module;
+  final String title;
+  final String dueDate;
+  final String submissionStatus;
+
+  const Assignment({
+    required this.module,
+    required this.title,
+    required this.dueDate,
+    required this.submissionStatus,
+  });
+}
+
+class SubmissionView extends StatefulWidget {
   const SubmissionView({Key? key}) : super(key: key);
 
+  @override
+  State<SubmissionView> createState() => _SubmissionViewState();
+}
+
+class _SubmissionViewState extends State<SubmissionView> {
+  final Assignment assignment = const Assignment(
+    module: 'Programming Applications and Programming Languages',
+    title: 'Item 1 (Flutter)',
+    dueDate: '31 July 2026 at 1:00 PM',
+    submissionStatus: 'Not Submitted',
+  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
